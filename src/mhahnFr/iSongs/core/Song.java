67a7c3246be29c23abdade5ae13af5d32ba90bd0
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-04-19 17:19:04 +0200

    Documented the song record

--- src/mhahnFr/iSongs/core/Song.java
+++ src/mhahnFr/iSongs/core/Song.java
@@ -3,21 +3,29 @@
  *
  * Copyright (C) 2024  mhahnFr
  *
- * This file is part of the iSongs-RadioText. This program is free software:
- * you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This file is part of the iSongs-RadioText.
  *
- * This program is distributed in the hope that it will be useful,
+ * iSongs-RadioText is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iSongs-RadioText is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * iSongs-RadioText, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package mhahnFr.iSongs.core;
 
+/**
+ * This record represents a song.
+ *
+ * @param title the title of the song
+ * @param interpreter the interpreter of the song
+ */
 public record Song(String title, String interpreter) {
 }
